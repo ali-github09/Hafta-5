fff996eb61c6c1275cd956a41defe0e32fe417f4
ali orkan <ali@ali-MacBook-Air.local>
2024-01-10 16:44:07 +0300

    try catch blogu

--- Main.java
+++ Main.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-    }
-}
+            Scanner input = new Scanner(System.in);
+            int[] arr = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+            System.out.print("Lütfen hangi dizi elemanını seçmek istediğinizi giriniz : ");
+
+            try {
+                int a = input.nextInt();
+                System.out.println(arr[a]);
+            } catch (Exception e) {
+                System.out.println("Yanlış bir ifade girdiniz");
+            }
+
+            }
+        }
+
